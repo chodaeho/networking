921e61bd409b091217b26e3c1859b58d2b39d3d7
daehocho <daehocho0637@gmail.com>
2021-12-23 18:00:54 +0900

    GCD 설명 및 실습

---
+++ GCD.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// Queue - Main, Global, Custom
+
+// - Main
+DispatchQueue.main.async {
+    // UI 업데이트에 사용, 사용자 인터렉션에 사용
+    let view = UIView()
+    view.backgroundColor = .green
+}
+
+// - Grobal
+
+// 복합적인 상황
+
+// Sync, Async
